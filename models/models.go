0ec960eecb993d010d3322e3664ac54409aa78a9
Obi Walker <obinnawalker@gmail.com>
2020-04-07 12:34:53 +0100

    make project modifications

--- models/models.go
+++ models/models.go
@@ -2,7 +2,6 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-
 type Country struct {
 	ID     primitive.ObjectID  `bson:"_id,omitempty"`
 	Country string			       `bson:"country,omitempty"`
